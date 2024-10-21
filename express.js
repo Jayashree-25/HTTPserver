6a76a904a3db7df145d7587b391ccbe9b49456ac
jayashre-25 <dasjayashree8260@gmail.com>
2024-10-21 09:54:28 +0530

    first commit

---
+++ express.js
@@ -0,0 +1,12 @@
+const express = require('express'); //The require function loads the Express module, allowing you to use its features
+const app = express(); //you create an instance of an Express application, this app will handle request and response
+const port = 3000; //This line sets a constant variable port to the number 3000, on which your application will listen for incoming requests. You can change this number if needed.
+
+app.get('/', function(req, res) { //This line defines a route for your application
+  res.send('Hello World!'); //this line sends the response 'Hello World!'
+});
+app.listen(port, function() { //This line starts the server and tells it to listen for incoming requests on the specified port, this runs when the server is successfully started
+  console.log(`Example app listening on port ${port}`); //${port} syntax is used to insert the value of the port variable into the message
+});
+// you can stop it by pressing Ctrl + C
+
