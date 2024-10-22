a6e7f77cab793fe996e3114efb54ef09b4cc3ca9
jayashre-25 <dasjayashree8260@gmail.com>
2024-10-22 22:07:41 +0530

    edited

--- express.js
+++ express.js
@@ -10,18 +10,111 @@
 // });
 // you can stop it by pressing Ctrl + C
 
+// < ----------- .............. -------------->
 // Creating a http server
 // express
 // node default library => NO 
+// const express = require('express');
+
+// const app1 = express();
+// function sum(n){
+//   let ans = 0;
+//   for(let i=1; i<=n; i++){
+//     ans = ans + i;
+//   }
+//   return ans;
+// }
+
+// app1.get("/", function(req, res) {
+//   const n = req.query.n;
+//   const ans = sum(n);
+//   res.send("hi your ans is " + ans);
+// });
+
+// app1.listen(3000, () => {
+//   console.log('Server is running on http://localhost:3000');
+// });
+
+
+//complicated one
+// const express = require('express');
+
+// const app1 = express();
+
+
+// app1.get("/", function(req, res) {
+
+//   res.send("hi");
+// });
+
+// app1.listen(3000, () => {
+//   console.log('Server is running on http://localhost:3000');
+// });
 const express = require('express');
 
-const app1 = express();
+const app2 = express();
+const users = [{
+  name: "Rahul",
+  kidneys: [{
+    healthy: false,
+  }]
+}];
 
-app1.get("/", function(req, res) {
-  res.send("hi there");
+app2.use(express.json());
+
+app2.get("/", function (req, res) {
+  const rahulKidneys = users[0].kidneys;
+  const noOfKidneys = rahulKidneys.length;
+  let noOfHealthyKidneys = 0;
+  for (let i = 0; i < rahulKidneys.length; i++) {
+    if (rahulKidneys[i].healthy) {
+      noOfHealthyKidneys = noOfHealthyKidneys + 1;
+    }
+  }
+  const noOfUnhealthyKidneys = noOfKidneys - noOfHealthyKidneys;
+  res.json({
+    noOfKidneys,
+    noOfHealthyKidneys,
+    noOfUnhealthyKidneys,
+  })
+  console.log(rahulKidneys);
+})
+
+app2.post("/", function (req, res) { //post => insert
+  const isHealthy = req.body.isHealthy;
+  users[0].kidneys.push({
+    healthy: isHealthy,
+  });
+  res.json({
+    msg: "Done!",
+  })
+})
+
+app2.put("/", function (req, res) {
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    users[0].kidneys[i].healthy = true; //makes all kidneys healthy
+  }
+  res.json({
+    msg: "Done",
+  })
+})
+
+//removing all the unhealthy kidneys
+app2.delete("/", function (req, res) {
+  const newKidneys = [];
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    if (users[0].kidneys[i].healthy) {
+      newKidneys.push({
+        healthy: true
+      })
+    }
+  }
+  users[0].kidneys = newKidneys;
+  res.json({
+    msg: "Done"
+  })
+})
+
+app2.listen(3000, () => {
+  console.log('server is running');
 });
-
-app1.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
-
