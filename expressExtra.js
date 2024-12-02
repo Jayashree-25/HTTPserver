5cf830c23019496b3f1e75462d04e004643470ca
jayashre-25 <dasjayashree8260@gmail.com>
2024-12-02 11:27:14 +0530

    removed

--- expressExtra.js
+++ expressExtra.js
@@ -1,30 +1,31 @@
-// const express = require("express");
-// const app = express();
-// const port = 3000;
-// app.get("/health-checkup", function (req, res) {
-//     const kidneyId = req.query.kidneyId;
-//     const username = req.headers.username;
-//     const password = req.headers.password;
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/health-checkup", function (req, res) {
+    const kidneyId = req.query.kidneyId;
+    const username = req.headers.username;
+    const password = req.headers.password;
 
-//     if (username != "Jayashree" || password != "pass") {
-//         res.status(403).json({
-//             msg: "user doesn't exist",
-//         });
-//         return;
-//     }
-//     if (kidneyId != 1 && kidneyId != 2) {
-//         res.status(411).json({
-//             msg: "wrong inputs",
-//         });
-//         return;
-//     }
-//     res.send("Your heart is healthy");
-// });
-// app.listen(port, () => {
-//     console.log("Server is running on http://localhost:3000");
-// });
+    if (username != "Jayashree" || password != "pass") {
+        res.status(403).json({
+            msg: "user doesn't exist",
+        });
+        return;
+    }
+    if (kidneyId != 1 && kidneyId != 2) {
+        res.status(411).json({
+            msg: "wrong inputs",
+        });
+        return;
+    }
+    res.send("Your heart is healthy");
+});
+app.listen(port, () => {
+    console.log("Server is running on http://localhost:3000");
+});
 
-//using middleware
+
+//USING MIDDLEWARE
 const express=require("express");
 const app1 = express();
 const port1 = 30001;
